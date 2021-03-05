90763d24c8d5fa0234a058e2101f91ae54d05a4c
Abdi Abdulqadir <aabdulqadir@roc-dev.com>
2021-03-05 11:59:26 +0100

    2.2 compleet

---
+++ h2/Kleuren.js
@@ -0,0 +1,21 @@
+var a = document.getElementById("blauw");{
+a.style.background="cyan";
+}
+var b = document.getElementById("groen");{
+    b.style.background="green";
+    b.style.position = "absolute"
+    b.style.top = "20px";
+    b.style.left = "100px"
+}
+var c = document.getElementById("geel");{
+    c.style.background="yellow";
+    c.style.position = "absolute"
+    c.style.top = "50px";
+    c.style.left = "200px"
+}
+var d = document.getElementById("rood");{
+    d.style.background="red";
+    d.style.position = "absolute"
+    d.style.top = "1px";
+    d.style.left = "300px"
+}
---
+++ h2/vulmetjs.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("paginatitel").innerText = "mijn geweldige pagina";
+document.getElementById("subkop").innerText = "mijn geweldige subkop";
+document.getElementById("aap1").src = "aap1.jpg "
+document.getElementById("aap2").src = "aap2.jpg"
